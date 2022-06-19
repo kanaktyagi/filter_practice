c6ccec2ff3553d592735bb08cdc52cc9c914a2db
Kanak Tyagi <tyagiknk.india@gmail.com>
2022-06-19 15:58:00 +0530

    Added the Filters for the site

--- src/components/Home/FilterPanel/index.js
+++ src/components/Home/FilterPanel/index.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { categoryList,ratingList } from '../../../constant'
+import CheckboxProton from '../../common/CheckboxProton'
+import FilterListToggle from '../../common/FilterListToggle'
+import './style.css'
 
-function FilterPannel() {
+function FilterPannel({selectedCategory, selectToggle,selectedRating,selectRating,cuisines,changeChecked}) {
   return (
-    <div>FilterPannel</div>
+    <>
+    <div className='input-group'>
+      <p className='label'>Category</p>
+     <FilterListToggle 
+          options={categoryList} 
+          value={selectedCategory}
+          selectToggle={selectToggle}/>
+    </div>
+    <div className='input-group'>
+      <p className='label'>Cuisine</p>
+   {cuisines.map(cuisine => <CheckboxProton
+     key ={cuisine.id} 
+     cuisine={cuisine} 
+     changeChecked={changeChecked}/>)}
+      </div>
+    <div className='input-group'>
+      <p className='label'>Star Rating</p>
+       <FilterListToggle
+         options={ratingList}
+         value={selectedRating}
+         selectToggle={selectRating}
+       />
+    </div>
+    </>
   )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FilterPannel from '../../components/Home/FilterPanel'
 import { List } from '../../components/Home/List'
 import SearchBar from '../../components/Home/SearchBar'
 import './style.css'
 
 function Home() {
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [selectedRating, setSelectedRating ] = useState()
+
+  const [cuisines, setCuisines] = useState([
+    { id: 1, checked: false, label: 'American' },
+    { id: 2, checked: false, label: 'Chinese' },
+    { id: 3, checked: false, label: 'Italian' },
+  ]);
+  const handleChangeChecked = (id) => {
+    const cuisinesStateList = cuisines
+    const changeCheckedCuisines = cuisinesStateList.map(item => item.id === id ?
+      {...item, checked: !item.checked}: item
+      )
+      setCuisines(changeCheckedCuisines)
+
+  }
+    const handleSelectedCategory = (event,value) =>  (
+      <div>
+         {!value ? null : setSelectedCategory(value)}
+      </div>
+     )
+  
+    const handleSelectedRating=(event,value) =>  (
+      <div>
+           {!value ? null : setSelectedRating(value)}
+      </div>
+     )
+
   return (
     <div className="home">
         {/*Search bar */}
         <SearchBar/>
         <div className='home_panelList-wrap'>
            <div className="home_panel-wrap">
-             <FilterPannel/>
-           </div>
+             <FilterPannel
+             cuisines={cuisines}
+              selectToggle={handleSelectedCategory} 
+              selectedCategory ={selectedCategory}
+              selectRating={handleSelectedRating}
+              selectedRating={selectedRating}
+              changeChecked={handleChangeChecked}
+               />
+           </div> 
             <div className="home_list-wrap">
              <List/>
             </div>
